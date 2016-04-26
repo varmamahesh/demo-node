f673c22fed9a5b12c902850a1078758da70d80fb
Mahesh Varma <varmamahesh@yahoo.com>
2016-04-26 16:31:19 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Demo App Green' });
+  res.render('index', { title: 'Demo App Blue' });
 });
 
 module.exports = router;
