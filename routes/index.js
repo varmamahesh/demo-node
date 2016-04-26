9c91a0b2eca26d795d2edf4893986685f7668b1c
Mahesh Varma <varmamahesh@yahoo.com>
2016-04-26 13:31:25 -0400

    New version Green

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Demo App Blue' });
+  res.render('index', { title: 'Demo App Green' });
 });
 
 module.exports = router;
